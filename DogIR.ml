e739821ce696b64997698f410d3285437101faf1
Nathan Chong <nathan.chong@gmail.com>
2015-03-15 22:04:14 +0000

    Pretty-printing gives proper AST (copy+pasteable into repl)

--- DogIR.ml
+++ DogIR.ml
@@ -84,50 +84,53 @@ let tr_oracle s =
 
 (* IR pretty printing *)
 
+let pp_string ppf s =
+  Format.fprintf ppf "\"%s\"" s
+
 let pp_oracle ppf = function
-| Oracle s -> Format.fprintf ppf "Oracle(%s)" s
-| OracleExists s -> Format.fprintf ppf "OracleExists(%s)" s
-| OracleTrue s -> Format.fprintf ppf "OracleTrue(%s)" s
+| Oracle s -> Format.fprintf ppf "Oracle(%a)" pp_string s
+| OracleExists s -> Format.fprintf ppf "OracleExists(%a)" pp_string s
+| OracleTrue s -> Format.fprintf ppf "OracleTrue(%a)" pp_string s
 
 let rec pp_actual ppf = function
 | EventActualOracle x -> Format.fprintf ppf "EventActualOracle(%a)" pp_oracle x
-| EventActualAttribute x -> Format.fprintf ppf "EventActualAttribute(%s)" x
+| EventActualAttribute x -> Format.fprintf ppf "EventActualAttribute(%a)" pp_string x
 | EventActualNot x -> Format.fprintf ppf "EventActualNot(%a)" pp_actual x
 
 let pp_startend ppf = function
-| AtStart -> Format.fprintf ppf "%@s"
-| AtEnd -> Format.fprintf ppf "%@e"
+| AtStart -> Format.fprintf ppf "AtStart"
+| AtEnd -> Format.fprintf ppf "AtEnd"
 
 let pp_star ppf = function
-| StarNone -> ()
-| Star -> Format.fprintf ppf "*"
-| StarPlus -> Format.fprintf ppf "*+"
-| StarMinus -> Format.fprintf ppf "*-"
-| StarNotPlus -> Format.fprintf ppf "*!-"
-| StarNotMinus -> Format.fprintf ppf "*!-"
+| StarNone -> Format.fprintf ppf "StarNone"
+| Star -> Format.fprintf ppf "Star"
+| StarPlus -> Format.fprintf ppf "StarPlus"
+| StarMinus -> Format.fprintf ppf "StarMinus"
+| StarNotPlus -> Format.fprintf ppf "StarNotPlus"
+| StarNotMinus -> Format.fprintf ppf "StarNotMinus"
 
 (* available in 4.02.0 *)
 let rec pp_print_list pp_v ppf = function
   | [] -> ()
-  | x::xs -> Format.fprintf ppf "%a " pp_v x; pp_print_list pp_v ppf xs
+  | x::xs -> Format.fprintf ppf "%a;" pp_v x; pp_print_list pp_v ppf xs
 
 let pp_event ppf = function
 | EventComplete -> Format.fprintf ppf "EventComplete"
-| Event (e,alist,se,star) -> Format.fprintf ppf "%s(%a)%a%a" e (pp_print_list pp_actual) alist pp_startend se pp_star star
+| Event (e,alist,se,star) -> Format.fprintf ppf "Event(%a,[%a],%a,%a)" pp_string e (pp_print_list pp_actual) alist pp_startend se pp_star star
 
 let pp_boolop ppf = function
-| BoolOr -> Format.fprintf ppf "||"
-| BoolAnd -> Format.fprintf ppf "&&"
-| BoolEq -> Format.fprintf ppf "=="
+| BoolOr -> Format.fprintf ppf "BoolOr"
+| BoolAnd -> Format.fprintf ppf "BoolAnd"
+| BoolEq -> Format.fprintf ppf "BoolEq"
 
 let rec pp_eventexpr ppf = function
-| ExprIdentifier x -> Format.fprintf ppf "%s" x
-| ExprOracle x -> Format.fprintf ppf "%a" pp_oracle x
-| ExprNum n -> Format.fprintf ppf "%d" n
-| ExprNot e -> Format.fprintf ppf "!(%a)" pp_eventexpr e
-| ExprBool (op,e1,e2) -> Format.fprintf ppf "(%a %a %a)" pp_eventexpr e1 pp_boolop op pp_eventexpr e2
-| ExprAssign (e1,e2) -> Format.fprintf ppf "(%a := %a)" pp_eventexpr e1 pp_eventexpr e2
-| ExprEvent e -> Format.fprintf ppf "%a" pp_event e
+| ExprIdentifier x -> Format.fprintf ppf "ExprIdentifier(%a)" pp_string x
+| ExprOracle x -> Format.fprintf ppf "ExprOracle(%a)" pp_oracle x
+| ExprNum n -> Format.fprintf ppf "ExprNum(%d)" n
+| ExprNot e -> Format.fprintf ppf "ExprNot(%a)" pp_eventexpr e
+| ExprBool (op,e1,e2) -> Format.fprintf ppf "ExprBool(%a,%a,%a)" pp_boolop op pp_eventexpr e1 pp_eventexpr e2
+| ExprAssign (e1,e2) -> Format.fprintf ppf "ExprAssign(%a,%a)" pp_eventexpr e1 pp_eventexpr e2
+| ExprEvent e -> Format.fprintf ppf "ExprEvent(%a)" pp_event e
 
 let print_eventexpr = pp_eventexpr Format.std_formatter
 
@@ -144,8 +147,8 @@ let string_of_oracle = function
 | OracleTrue x -> Format.sprintf "%s?#" x
 
 let string_of_startend = function
-| AtStart -> "@s"
-| AtEnd   -> "@e"
+| AtStart -> "%@s"
+| AtEnd   -> "%@e"
 
 let string_of_star = function
 | StarNone -> ""
@@ -154,11 +157,6 @@ let string_of_star = function
 | StarMinus -> "*-"
 | StarNotPlus -> "*!+"
 | StarNotMinus -> "*!-"
-
-let rec string_of_oracle = function
-| Oracle x -> x
-| OracleExists x -> Format.sprintf "%s#" x
-| OracleTrue x -> Format.sprintf "%s?" x
 
 let rec string_of_actual = function
 | EventActualOracle x -> string_of_oracle x
