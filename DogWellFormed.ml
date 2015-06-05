c91c6a38af2877abdd064290e2126318514f2f0a
Nathan Chong <nathan.chong@gmail.com>
2015-06-05 13:23:58 +0100

    Add new well-formed condition

--- DogWellFormed.ml
+++ DogWellFormed.ml
@@ -69,7 +69,7 @@ let check_at_most_one_star_per_path dog =
     let events = events_of_path path in
     let stars = List.filter (function Event(_,_,_,Star) -> true | _ -> false) events in
     if List.length stars <= 1 then ()
-    else (Printf.printf "More than one star event on path\n"; ok := false)
+    else (Printf.printf "Error: more than one star event on path\n"; ok := false)
   in
   let _ = List.iter (fun i ->
     let paths = extract_paths rules i accepting in
@@ -87,7 +87,7 @@ let check_no_repeating_events dog =
     let edges = outcoming_edges_of_state dog state in
     let events = List.fold_right (fun e acc -> (events_of_eventexpr e) @ acc) edges [] in
     if List.length (nodups events) == List.length events then ()
-    else (Printf.printf "Bad state %s\n" state; ok := false)
+    else (Printf.printf "Error: bad state '%s' has same event on different transitions\n" state; ok := false)
   in
   let _ = G.iter_vertex check_state dog.rules in
   !ok
@@ -103,7 +103,7 @@ let check_no_conjuncted_events dog =
         let events_of_e1 = events_of_eventexpr e1 in
         let events_of_e2 = events_of_eventexpr e2 in
         if events_of_e1 <> [] && events_of_e2 <> [] then
-          (Printf.printf "Events appear in conjunct %s\n" (string_of_eventexpr eventexpr); ok := false));
+          (Printf.printf "Error: events appear in conjunct %s\n" (string_of_eventexpr eventexpr); ok := false));
       check_eventexpr e1; check_eventexpr e2
     | ExprAssign (e1, e2) -> (check_eventexpr e1; check_eventexpr e2)
     | _ -> ()
@@ -111,6 +111,21 @@ let check_no_conjuncted_events dog =
   let _ = G.iter_edges_e (fun (_,eventexpr,_) -> check_eventexpr eventexpr) dog.rules in
   !ok
 
+(* Every state appearing in an assertion is reachable by exactly one initial state *)
+let check_exactly_one_initial_state dog =
+  let ok = ref true in
+  let rules = dog.rules in
+  let is_path = make_path_checker rules in
+  let initial_states = initial_states_of dog in
+  let assert_states = assert_states_of dog in
+  let _ =
+    List.iter (fun s ->
+      let init_candidates = List.filter (fun init -> is_path init s) initial_states in
+      if List.length init_candidates == 1 then ()
+      else (Printf.printf "Error: assert state '%s' reachable from initial states [%s]\n" s (String.concat ", " init_candidates); ok := false)
+    ) assert_states
+  in
+  !ok
 
 (* List of checks and accompanying error message *)
 let checks = [(check_initial_states, "Initial state statement are not well-defined\n");
@@ -119,6 +134,7 @@ let checks = [(check_initial_states, "Initial state statement are not well-defin
               (check_at_most_one_star_per_path, "Bad path with >1 star event\n");
               (check_no_repeating_events, "State with same event on different edges\n");
               (check_no_conjuncted_events, "Edge with conjunction of events\n");
+              (check_exactly_one_initial_state, "State in assert reachable from multiple initial states\n");
              ]
 
 let warn_all_initial_states_labelled dog =
